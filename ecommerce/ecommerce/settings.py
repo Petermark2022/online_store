8a242052b4a6339f383a18f8d536f51d0877c8f3
PETERMARK IKPEAZU <p.ikpeazu.22@abdn.ac.uk>
2023-05-04 19:56:45 +0000

    update to requirement

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -145,5 +145,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
